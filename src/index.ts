30d23fa7b25c74cb531cc807397b45aa4e4d98fb
josebuschiazzo2 <104038315+josebuschiazzo2@users.noreply.github.com>
2022-05-06 12:18:40 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import "./styles.css";
+let counter: number = 0;
+let total_lap: number =0;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+while (counter<4) {
+  let lap:number = Number(prompt ("Ingrese tiempo de la vuelta " + counter));
+  total_lap+=lap;                        
+  counter++;
+ }
+console.clear();
+console.log("El tiempo total fue de: " + total_lap + ". Con un tiempo promedio de: " + total_lap/4);
